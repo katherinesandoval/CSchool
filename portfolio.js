bcf23059e83ef5baa63f087f21d99380bacdc260
Katherine Sandoval <kate_sandoval27@hotmail.com>
2014-08-28 12:26:15 -0400

    Citizen School
    Citizen School Curriculum

---
+++ portfolio.js
@@ -0,0 +1,38 @@
+document.getElementById("btn").onclick = function () {
+    location.href = "https://www.linkedin.com/in/katherinesandoval";
+};
+document.getElementById("btn1").onclick = function () {
+    location.href = "C://Users/katerine/Documents/google/goog2.html";
+};
+document.getElementById("btn2").onclick = function () {
+    location.href = "C://Users/katerine/Documents/google/goog2.html";
+};
+document.getElementById("btn3").onclick = function () {
+    location.href = "https://www.facebook.com/katherine.sandoval.399";
+};
+document.getElementById("btn5").onclick = function () {
+    location.href = "C://Users/katerine/Desktop/Citizen%20School/button1.html";
+};
+
+document.getElementById("btn7").onclick = function () {
+    location.href = "C://Users/katerine/Desktop/instagram-gravity-gallery/index1.html";
+
+};
+
+document.getElementById("btn4").onclick = function () {
+    location.href = "C://Users/katerine/Desktop/Citizen%20School/aboutMe.html";
+
+};
+
+document.getElementById("btn6").onclick = function () {
+    location.href = "file:///C://Users/katerine/Desktop/Citizen%20School/project.html";
+
+};
+
+
+/*document.getElementById("btn").onclick = function () {
+    location.href = "https://twitter.com/Violettycatty";
+    };
+     document.getElementById("btn1").onclick =function() {
+      location.href = "https://www.linkedin.com/profile/view?id=250007829&trk=nav_responsive_tab_profile_pic";
+    };*/
