b577a74fdc5a425f95ea7e56a39a9086341fa33c
Katherine Sandoval <kate_sandoval27@hotmail.com>
2014-08-28 19:31:54 -0400

    .

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -13,26 +13,20 @@ document.getElementById("btn3").onclick = function () {
 document.getElementById("btn5").onclick = function () {
     location.href = "C://Users/katerine/Desktop/Citizen%20School/button1.html";
 };
-
 document.getElementById("btn7").onclick = function () {
     location.href = "C://Users/katerine/Desktop/instagram-gravity-gallery/index1.html";
-
 };
-
 document.getElementById("btn4").onclick = function () {
     location.href = "C://Users/katerine/Desktop/Citizen%20School/aboutMe.html";
-
 };
-
 document.getElementById("btn6").onclick = function () {
     location.href = "file:///C://Users/katerine/Desktop/Citizen%20School/project.html";
-
 };
 
 
 /*document.getElementById("btn").onclick = function () {
     location.href = "https://twitter.com/Violettycatty";
-    };
-     document.getElementById("btn1").onclick =function() {
+  };
+  document.getElementById("btn1").onclick =function() {
       location.href = "https://www.linkedin.com/profile/view?id=250007829&trk=nav_responsive_tab_profile_pic";
-    };*/
+};*/
